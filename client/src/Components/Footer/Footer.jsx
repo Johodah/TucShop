c8336db1b4185ce0177ba9b412f72ca148b88bec
Beowulf1337 <oliver.mantorp@gmail.com>
2024-12-03 16:12:46 +0100

    added code which makes the page logo show up in the footer and make it clickable brining you to the /home page aswell as added router to make the navigation between pages work aswell as updated the css to make the logo fit better

--- client/src/Components/Footer/Footer.jsx
+++ client/src/Components/Footer/Footer.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './Footer.css';
 import SocialMediaLinks from './SocialMediaLinks';
+import PageLogoLink from './PageLogoLink'; 
 
 const Footer = () => {
   return (
     <footer className="Footer">
+      <PageLogoLink />
       <SocialMediaLinks />
       <p className="Footer-text"></p>
     </footer>
---
+++ client/src/Components/Footer/PageLogoLink.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import pageLogo from '../../Images/CUT-logo.png';
+
+const PageLogoLink = () => {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate('/Home');
+  };
+
+  return (
+    <div className="pageLogoContainer"> 
+      <img
+        src={pageLogo}
+        alt="Go to Home"
+        onClick={handleClick}
+        className="Page-Logo"
+      />
+    </div>
+  );
+};
+
+export default PageLogoLink;
