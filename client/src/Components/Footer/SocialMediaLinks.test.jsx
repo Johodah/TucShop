3349ae2f01c6a8940e4b892d6affa44386c97b6b
Beowulf1337 <oliver.mantorp@gmail.com>
2024-12-03 12:36:25 +0100

    added import for jest

--- client/src/Components/Footer/SocialMediaLinks.test.jsx
+++ client/src/Components/Footer/SocialMediaLinks.test.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import SocialMediaLinks from './SocialMediaLinks';
+import '@testing-library/jest-dom';
 
 describe('SocialMediaLinks Component', () => {
   beforeAll(() => {
