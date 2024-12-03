662252a9ad3a86f6aba056c3f8efcf9176f51016
Beowulf1337 <oliver.mantorp@gmail.com>
2024-12-03 14:10:46 +0100

    more changes

--- client/src/Components/Footer/SocialMediaLinks.test.jsx
+++ client/src/Components/Footer/SocialMediaLinks.test.jsx
@@ -1,56 +1,56 @@
-import { render, screen, waitFor } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';  // Ensure this import is correct
+import { render, screen, fireEvent } from '@testing-library/react';
 import SocialMediaLinks from './SocialMediaLinks';
 
 describe('SocialMediaLinks Component', () => {
   beforeEach(() => {
+    delete global.open;
     global.open = jest.fn();
   });
 
-  it('should navigate to the correct Tiktok page when the Tiktok image is clicked', async () => {
+  it('should navigate to the correct Tiktok page when the Tiktok image is clicked', () => {
     render(<SocialMediaLinks />);
-    const tiktokImage = await screen.findByAltText('Tiktok');
-    userEvent.click(tiktokImage);
+    const tiktokLink = screen.getByAltText('Tiktok').closest('a');
+    fireEvent.click(tiktokLink);
     expect(global.open).toHaveBeenCalledWith(
       'https://www.tiktok.com/discover/tuc-h%C3%B6gskola-j%C3%B6nk%C3%B6ping',
       '_blank'
     );
   });
 
-  it('should navigate to the correct Youtube page when the Youtube image is clicked', async () => {
+  it('should navigate to the correct Youtube page when the Youtube image is clicked', () => {
     render(<SocialMediaLinks />);
-    const youtubeImage = await screen.findByAltText('Youtube');
-    userEvent.click(youtubeImage);
+    const youtubeLink = screen.getByAltText('Youtube').closest('a');
+    fireEvent.click(youtubeLink);
     expect(global.open).toHaveBeenCalledWith(
       'https://www.youtube.com/user/TUCtelevision',
       '_blank'
     );
   });
 
-  it('should navigate to the correct Linkedin page when the Linkedin image is clicked', async () => {
+  it('should navigate to the correct Linkedin page when the Linkedin image is clicked', () => {
     render(<SocialMediaLinks />);
-    const linkedinImage = await screen.findByAltText('linkedin'); 
-    userEvent.click(linkedinImage);
+    const linkedinLink = screen.getByAltText('linkedin').closest('a');
+    fireEvent.click(linkedinLink);
     expect(global.open).toHaveBeenCalledWith(
       'https://www.linkedin.com/company/tuc-sweden-ab/?originalSubdomain=se',
       '_blank'
     );
   });
 
-  it('should navigate to the correct Instagram page when the Instagram image is clicked', async () => {
+  it('should navigate to the correct Instagram page when the Instagram image is clicked', () => {
     render(<SocialMediaLinks />);
-    const instagramImage = await screen.findByAltText('Instagram');
-    userEvent.click(instagramImage);
+    const instagramLink = screen.getByAltText('Instagram').closest('a');
+    fireEvent.click(instagramLink);
     expect(global.open).toHaveBeenCalledWith(
       'https://www.instagram.com/tucyrkeshogskola/',
       '_blank'
     );
   });
 
-  it('should navigate to the correct Facebook page when the Facebook image is clicked', async () => {
+  it('should navigate to the correct Facebook page when the Facebook image is clicked', () => {
     render(<SocialMediaLinks />);
-    const facebookImage = await screen.findByAltText('Facebook');
-    userEvent.click(facebookImage);
+    const facebookLink = screen.getByAltText('Facebook').closest('a');
+    fireEvent.click(facebookLink);
     expect(global.open).toHaveBeenCalledWith(
       'https://www.facebook.com/tucsweden/?locale2=en_GB&_rdr',
       '_blank'
