835827ab7ece513c152c18da2f7031b34daa0e5c
Beowulf1337 <oliver.mantorp@gmail.com>
2024-12-03 16:12:46 +0100

    added code which makes the page logo show up in the footer and make it clickable brining you to the /home page aswell as added router to make the navigation between pages work aswell as updated the css to make the logo fit better

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { BrowserRouter as Router } from 'react-router-dom';  // Import Router
 import Footer from './Components/Footer/Footer'; 
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
+    <Router> 
     <>
       <div>
         <a href="https://vite.dev" target="_blank">
@@ -33,6 +35,7 @@ function App() {
       <Footer />
       </div>
     </>
+    </Router>
   )
 }
 
