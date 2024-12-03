8bb812ec7212ada9ccffe02e9d61edbd44afad29
Johodah <johnny.dahle@hotmail.com>
2024-12-03 15:09:49 +0100

    refactored code to make it more CC.

--- Server.Test/Database.Test/DatabaseTests.cs
+++ Server.Test/Database.Test/DatabaseTests.cs
@@ -22,7 +22,32 @@ namespace Server.Test.Database.Test
         [Fact]
         public void CanAddCustomer()
         {
-            var customer = new Customer
+            var customer = CreateTestCustomer();
+
+            _context.Customers.Add(customer);
+            _context.SaveChanges();
+
+            var savedCustomer = _context.Customers.FirstOrDefault(c => c.CustomerEmail == customer.CustomerEmail);
+            Assert.NotNull(savedCustomer);
+            Assert.Equal(customer.CustomerName, savedCustomer.CustomerName);
+        }
+
+        [Fact]
+        public void CanAddProduct()
+        {
+            var product = CreateTestProduct();
+
+            _context.Products.Add(product);
+            _context.SaveChanges();
+
+            var savedProduct = _context.Products.FirstOrDefault(p => p.ProductName == product.ProductName);
+            Assert.NotNull(savedProduct);
+            Assert.Equal(product.ProductDescription, savedProduct.ProductDescription);
+        }
+
+        private Customer CreateTestCustomer()
+        {
+            return new Customer
             {
                 CustomerName = "Test User",
                 CustomerEmail = "test.user@example.com",
@@ -32,18 +57,11 @@ namespace Server.Test.Database.Test
                 CreatedAt = new DateOnly(2021, 5, 1),
                 UpdatedAt = new DateOnly(2021, 5, 1)
             };
-
-            _context.Customers.Add(customer);
-            _context.SaveChanges();
-
-            var savedCustomer = _context.Customers.FirstOrDefault(c => c.CustomerEmail == "test.user@example.com");
-            Assert.NotNull(savedCustomer);
-            Assert.Equal("Test User", savedCustomer.CustomerName);
         }
-        [Fact]
-        public void CanAddProduct()
+
+        private Product CreateTestProduct()
         {
-            var product = new Product
+            return new Product
             {
                 ProductName = "Test Product",
                 ProductDescription = "Test Description",
@@ -53,11 +71,6 @@ namespace Server.Test.Database.Test
                 CreatedAt = new DateOnly(2021, 5, 1),
                 UpdatedAt = new DateOnly(2021, 5, 1)
             };
-            _context.Products.Add(product);
-            _context.SaveChanges();
-            var savedProduct = _context.Products.FirstOrDefault(p => p.ProductName == "Test Product");
-            Assert.NotNull(savedProduct);
-            Assert.Equal("Test Description", savedProduct.ProductDescription);
         }
     }
 }
