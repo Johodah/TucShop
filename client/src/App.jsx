a5358c22f617364b908c3d31f44beaf4d717c4f9
nikospr90 <nick_prevenios@hotmail.com>
2024-12-03 18:37:45 +0100

    Added first test to routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import React from "react";
+import { Navlink, Route, Routes, Router, Link } from "react-router-dom";
 import "./App.css";
 import Header from "./Pages/Components/Header";
 
@@ -9,20 +9,25 @@ function App() {
 
   return (
     <>
-      <div>
-        <Header />
-      </div>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+      <Router>
+        <div>
+          <Header />
+        </div>
+        <div className="card">
+          <button onClick={() => setCount((count) => count + 1)}>
+            count is {count}
+          </button>
+          <p>
+            Edit <code>src/App.jsx</code> and save to test HMR
+          </p>
+        </div>
+        <p className="read-the-docs">
+          Click on the Vite and React logos to learn more
         </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <Routes>
+          <Route path="/" element={<h1>Home</h1>} />
+        </Routes>
+      </Router>
     </>
   );
 }
--- client/src/Pages/Tests/Page_Test.test.jsx
+++ client/src/Pages/Tests/Page_Test.test.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Header from "../Components/Header";
 import Logo from "../Components/Modules/Logo";
 import { describe, test, expect } from "vitest";
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 
 describe("Header Component", () => {
@@ -14,5 +14,13 @@ describe("Header Component", () => {
   test("should render the Header component", () => {
     render(<Header />);
     expect(screen.getByAltText("Home")).toBeInTheDocument();
+  });
+});
+
+describe("Routing Tests", () => {
+  test("should render the Homepage", () => {
+    render(<HomePage />);
+    fireEvent.click(screen.getByAltText("Home"));
+    expect(window.location.pathname).toBe("/");
   });
 });
