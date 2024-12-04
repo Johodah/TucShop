2c1e471fe53a53edee265b635aa8c7c1176239ac
Beowulf1337 <oliver.mantorp@gmail.com>
2024-12-04 12:22:43 +0100

    added more tests for future clickable text links

--- client/src/Components/Footer/LinkToServicesFooter.test.jsx
+++ client/src/Components/Footer/LinkToServicesFooter.test.jsx
@@ -18,4 +18,49 @@ describe('Contact Us Link', () => {
 
     expect(window.location.pathname).toBe('/Contact');
   });
+
+  test('navigates to the correct external URL when About the Website link is clicked', () => {
+    render(
+      <Router>
+        <LinkToServicesFooter />
+      </Router>
+    );
+
+    const aboutWebsiteLink = screen.getByRole('link', { name: 'About the Website Cookie policy' });
+
+    fireEvent.click(aboutWebsiteLink);
+
+    expect(aboutWebsiteLink).toHaveAttribute('href', 'https://www.tucsweden.se/cookiepolicy-tuc-yrkeshoskola/'); 
+    expect(aboutWebsiteLink).toHaveAttribute('target', '_blank');
+  });
+
+  test('navigates to the correct external URL when Personal data policy link is clicked', () => {
+    render(
+      <Router>
+        <LinkToServicesFooter />
+      </Router>
+    );
+
+    const personalDataPolicyLink = screen.getByRole('link', { name: 'Personal data policy' });
+
+    fireEvent.click(personalDataPolicyLink);
+
+    expect(personalDataPolicyLink).toHaveAttribute('href', 'https://www.tucsweden.se/personuppgiftspolicy/'); 
+    expect(personalDataPolicyLink).toHaveAttribute('target', '_blank');
+  });
+
+  test('navigates to the correct external URL when Log in to ForgetPoint link is clicked', () => {
+    render(
+      <Router>
+        <LinkToServicesFooter />
+      </Router>
+    );
+
+    const loginForgetPointLink = screen.getByRole('link', { name: 'Log in to ForgetPoint' });
+
+    fireEvent.click(loginForgetPointLink);
+
+    expect(loginForgetPointLink).toHaveAttribute('href', 'https://tucsweden.learnpoint.se/LoginForms/LoginForm.aspx?ReturnUrl=%2Fdefault.aspx'); // Adjust the URL as needed
+    expect(loginForgetPointLink).toHaveAttribute('target', '_blank');
+  });
 });
