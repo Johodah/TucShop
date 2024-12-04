50f8ca75481f8ad7f5ad649be0dfe7fbbcfbd1c6
Beowulf1337 <oliver.mantorp@gmail.com>
2024-12-04 09:37:09 +0100

    tried to update the test logic for LinkToServiceFooter to be more like the one for the home page navigation

--- client/src/Components/Footer/LinkToServicesFooter.test.jsx
+++ client/src/Components/Footer/LinkToServicesFooter.test.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom';
-import { MemoryRouter } from 'react-router-dom'; 
+import { BrowserRouter as Router } from 'react-router-dom'; 
 import LinkToServicesFooter from './LinkToServicesFooter';
 
 describe('Contact Us Link', () => {
   test('navigates to the correct page when clicked', () => {
     render(
-      <MemoryRouter>
+      <Router>
         <LinkToServicesFooter />
-      </MemoryRouter>
+      </Router>
     );
 
     const contactLink = screen.getByRole('link', { name: 'Contact Us' });
