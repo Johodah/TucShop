82d9af0b1868a344fd3e42dd720641ed42dc6749
Beowulf1337 <oliver.mantorp@gmail.com>
2024-12-04 11:20:03 +0100

    removed uncecessary code from SocialMediaLinks.jsx ( rel="noopener noreferrer")

--- client/src/Components/Footer/SocialMediaLinks.jsx
+++ client/src/Components/Footer/SocialMediaLinks.jsx
@@ -9,19 +9,19 @@ import facebookLogo from '../../Images/facebook-logo.png';
 const SocialMediaLinks = () => {
   return (
     <div className = "SocialMediaLogoContainer">
-      <a href="https://www.tiktok.com/discover/tuc-h%C3%B6gskola-j%C3%B6nk%C3%B6ping" target="_blank" rel="noopener noreferrer">
+      <a href="https://www.tiktok.com/discover/tuc-h%C3%B6gskola-j%C3%B6nk%C3%B6ping" target="_blank">
         <img src={tiktokLogo} alt="Tiktok" className="SocialMedia-logo" />
       </a>
-      <a href="https://www.youtube.com/user/TUCtelevision" target="_blank" rel="noopener noreferrer">
+      <a href="https://www.youtube.com/user/TUCtelevision" target="_blank">
         <img src={youtubeLogo} alt="Youtube" className="SocialMedia-logo" />
       </a>
-      <a href="https://www.linkedin.com/company/tuc-sweden-ab/?originalSubdomain=se" target="_blank" rel="noopener noreferrer">
+      <a href="https://www.linkedin.com/company/tuc-sweden-ab/?originalSubdomain=se" target="_blank">
         <img src={linkedinLogo} alt="linkedin" className="SocialMedia-logo" />
       </a>
-      <a href="https://www.instagram.com/tucyrkeshogskola/" target="_blank" rel="noopener noreferrer">
+      <a href="https://www.instagram.com/tucyrkeshogskola/" target="_blank">
         <img src={instagramLogo} alt="Instagram" className="SocialMedia-logo" />
       </a>
-      <a href="https://www.facebook.com/tucsweden/?locale2=en_GB&_rdr" target="_blank" rel="noopener noreferrer">
+      <a href="https://www.facebook.com/tucsweden/?locale2=en_GB&_rdr" target="_blank">
         <img src={facebookLogo} alt="Facebook" className="SocialMedia-logo" />
       </a>
     </div>
