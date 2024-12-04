a498452917043cf611b42d26fb39fad31eb822ad
Beowulf1337 <oliver.mantorp@gmail.com>
2024-12-04 23:34:32 +0100

    merged footer and development

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,14 @@
-import { useState } from 'react'
+import { useState } from 'react';
 import React from "react";
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { BrowserRouter as Router } from 'react-router-dom'; 
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; 
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
 import Homepage from "./Pages/HomePage";
 import Checkout from "./Pages/Checkout";
 import Header from "./Pages/Components/Header";
 import Footer from './Pages/Components/Footer'; 
+
 function App() {
   return (
     <>
