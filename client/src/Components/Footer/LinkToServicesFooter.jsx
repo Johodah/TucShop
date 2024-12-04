0d5769c967c31a4d4654b3c82df5d3736dd5d6b4
Beowulf1337 <oliver.mantorp@gmail.com>
2024-12-04 11:16:35 +0100

    fixed the rest off the text for the LinkToServicesFooter.jsx and fixed css so it looks nicer when in full screen mode

--- client/src/Components/Footer/LinkToServicesFooter.jsx
+++ client/src/Components/Footer/LinkToServicesFooter.jsx
@@ -2,20 +2,31 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const LinkToServicesFooter = () => {
-  return (
-    <div className="LinksToServicesTextFooter">
-      <p>We at CUT</p>
-      <p>Whistle blowing</p>
-      <p>Graphic manual</p>
-      <div className="LinkTextFooterContactUs">
-        <Link to="/Contact">
-          <div className="ContactUsTextLink">
-            Contact Us
-          </div>
-        </Link>
-      </div>
-    </div>
-  );
+    return (
+        <div className="LinksToServicesTextContainer">
+            <div className="LinksToServicesTextFooter1">
+                <p>We at CUT</p>
+                <p>Whistle blowing</p>
+                <p>Graphic manual</p>
+                <div className="LinkTextFooterContactUs">
+                    <Link to="/Contact">
+                        <div className="ContactUsTextLink">
+                            Contact Us
+                        </div>
+                    </Link>
+                </div>
+            </div>
+            <div className="LinksToServicesTextFooter2">
+            <p>About the Website
+            Cookie policy</p>
+            <p>Personal data policy</p>
+            <p>View and update your cookie consent</p>
+            </div>
+            <div className="LinksToServicesTextFooter3">
+            <p>Log in to ForgetPoint</p>
+            </div>
+        </div>
+    );
 };
 
 export default LinkToServicesFooter;
