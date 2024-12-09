8d8f12d8d7422d8290e61db4e71e73ec48beee2b
Beowulf1337 <oliver.mantorp@gmail.com>
2024-12-09 15:23:46 +0100

    a basic attempt at designing a checkout page using mock data

--- client/src/Pages/Checkout.jsx
+++ client/src/Pages/Checkout.jsx
@@ -1,9 +1,25 @@
 import React from "react";
+import mockData from '/src/Pages/Components/Modules/mockData.json';
+import './Components/Modules/Checkout.css';
 
 const Checkout = () => {
+  const cartItems = mockData.data; 
+
+  const totalPrice = cartItems.reduce((total, item) => total + item.price, 0);
+
   return (
-    <div>
-      <h1>Checkout</h1>
+    <div className="checkout">
+      <div className="cartItems">
+        {cartItems.map((item) => (
+          <div key={item.productId} className="cart-item">
+            <h3 className="cart-item-name">{item.name}</h3>
+            <img src={item.image} alt={item.name} style={{ width: "100px" }} />
+            <p>Price: {item.price} kr</p>
+          </div>
+        ))}
+        <h2>Total: {totalPrice} kr</h2>
+        <button className="purchaseButton">Purchase</button>
+      </div>
     </div>
   );
 };
