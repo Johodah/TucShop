b0766c2d5b9bdeb05c917bdb5e0678307f4b5c0b
nikospr90 <nick_prevenios@hotmail.com>
2024-12-10 13:27:36 +0100

    Configuration for the db connection in the frontend

--- client/src/Pages/Components/ProductCard.jsx
+++ client/src/Pages/Components/ProductCard.jsx
@@ -4,7 +4,17 @@ import AddToCart from "../Components/Modules/AddToCartButton/AddtoCartButton";
 import { useNavigate } from "react-router-dom";
 
 function ProductCard() {
-  const incomingData = mockData;
+  const [incomingData, setIncomingData] = React.useState([]);
+
+  React.useEffect(() => {
+    fetch("http://localhost:7181/api/Products")
+      .then((res) => res.json())
+      .then((data) => setIncomingData(data))
+      .catch((error) => console.log(error));
+  }, []);
+  console.log(incomingData);
+
+  // const incomingData = mockData;
   const navigate = useNavigate();
   const handleClick = (item) => {
     navigate("/product", { state: { product: item } });
@@ -12,20 +22,21 @@ function ProductCard() {
 
   return (
     <div className="mainContent">
-      {incomingData.data.map((item) => {
-        return (
-          <button onClick={() => handleClick(item)} key={item.productId}>
-            <div className="courseContainer">
-              <h3>{item.name}</h3>
-              <img src={item.image} alt={"course"} />
-              <p>{item.shortDescription}</p>
-              <p>{item.stock} spot(s)</p>
-              <p>{item.price} kr</p>
-              <AddToCart stock={item.stock} productId={item.productId} />
-            </div>
-          </button>
-        );
-      })}
+      {incomingData.length > 0 &&
+        incomingData.map((item) => {
+          return (
+            <button onClick={() => handleClick(item)} key={item.productId}>
+              <div className="courseContainer">
+                <h3>{item.name}</h3>
+                <img src={item.image} alt={"course"} />
+                <p>{item.shortDescription}</p>
+                <p>{item.stock} spot(s)</p>
+                <p>{item.price} kr</p>
+                <AddToCart stock={item.stock} productId={item.productId} />
+              </div>
+            </button>
+          );
+        })}
     </div>
   );
 }
